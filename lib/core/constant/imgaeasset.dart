10e3381b3e62eff1658c89551aa64d502999586c
ShadenD <shadenduaibes@gmail.com>
2024-05-06 10:09:16 +0300

    first commit

--- lib/core/constant/imgaeasset.dart
+++ lib/core/constant/imgaeasset.dart
@@ -1,6 +1,5 @@
 class AppImageAsset {
   static const String rootImages = "assets/images";
-
   static const String onBoardingImageOne = "$rootImages/one.png";
   static const String onBoardingImageTwo = "$rootImages/two.png";
   static const String onBoardingImageThree = "$rootImages/three.png";
